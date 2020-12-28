67280e2af67ebd4442344f3b7059596b0d6257a4
Joshua Wallace <JoshuaStarkWallace@outlook.com>
2020-12-28 12:46:40 -0700

    2nd commit

---
+++ Music_Player_App/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Music_Player_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MediaPlayerComponent = new AxWMPLib.AxWindowsMediaPlayer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MediaPlayerComponent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MediaPlayerComponent
+            // 
+            this.MediaPlayerComponent.Enabled = true;
+            this.MediaPlayerComponent.Location = new System.Drawing.Point(0, 149);
+            this.MediaPlayerComponent.Name = "MediaPlayerComponent";
+            this.MediaPlayerComponent.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("MediaPlayerComponent.OcxState")));
+            this.MediaPlayerComponent.Size = new System.Drawing.Size(584, 112);
+            this.MediaPlayerComponent.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 227);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.BackColor = System.Drawing.Color.Gray;
+            this.BrowseButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BrowseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BrowseButton.ForeColor = System.Drawing.Color.White;
+            this.BrowseButton.Location = new System.Drawing.Point(237, 25);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(105, 41);
+            this.BrowseButton.TabIndex = 2;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = false;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.BackColor = System.Drawing.Color.Gray;
+            this.PauseButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.PauseButton.Font = new System.Drawing.Font("Webdings", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.PauseButton.ForeColor = System.Drawing.Color.White;
+            this.PauseButton.Location = new System.Drawing.Point(169, 86);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(105, 41);
+            this.PauseButton.TabIndex = 3;
+            this.PauseButton.Text = ";";
+            this.PauseButton.UseVisualStyleBackColor = false;
+            this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.BackColor = System.Drawing.Color.Gray;
+            this.PlayButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.PlayButton.Font = new System.Drawing.Font("Webdings", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.PlayButton.ForeColor = System.Drawing.Color.White;
+            this.PlayButton.Location = new System.Drawing.Point(31, 89);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(105, 41);
+            this.PlayButton.TabIndex = 4;
+            this.PlayButton.Text = "4";
+            this.PlayButton.UseVisualStyleBackColor = false;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.BackColor = System.Drawing.Color.Gray;
+            this.StopButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.StopButton.Font = new System.Drawing.Font("Webdings", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.StopButton.ForeColor = System.Drawing.Color.White;
+            this.StopButton.Location = new System.Drawing.Point(307, 89);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(105, 41);
+            this.StopButton.TabIndex = 5;
+            this.StopButton.Text = "g";
+            this.StopButton.UseVisualStyleBackColor = false;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.Gray;
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ExitButton.Font = new System.Drawing.Font("Webdings", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.ExitButton.ForeColor = System.Drawing.Color.White;
+            this.ExitButton.Location = new System.Drawing.Point(443, 89);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(105, 41);
+            this.ExitButton.TabIndex = 6;
+            this.ExitButton.Text = "r";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.DimGray;
+            this.textBox2.Location = new System.Drawing.Point(4, 230);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(105, 26);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Music Player";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.MediaPlayerComponent);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Music Player";
+            ((System.ComponentModel.ISupportInitialize)(this.MediaPlayerComponent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer MediaPlayerComponent;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Music_Player_App/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music_Player_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+
+            MediaPlayerComponent.URL = textBox1.Text;
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            MediaPlayerComponent.Ctlcontrols.play();
+        }
+
+        private void PauseButton_Click(object sender, EventArgs e)
+        {
+            MediaPlayerComponent.Ctlcontrols.pause();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            MediaPlayerComponent.Ctlcontrols.stop();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
